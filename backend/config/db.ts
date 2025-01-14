07048b9b5e19fb9d552412d515fc71e9fa06145b
prabhjotsumman <prabhjot.nith@gmail.com>
2025-01-14 14:47:06 -0700

    server Timeout time increased to 50k

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -4,10 +4,15 @@ const connectDB = async () => {
   const MONGO_URI =
     "mongodb+srv://prabh-user:prabh-imdb@cluster0.fh3m8.mongodb.net/";
   try {
+    const conn = await mongoose.connect(MONGO_URI || process.env.MONGO_URI!, {
+      connectTimeoutMS: 50000, // Adjust timeout
+    });
+
     await mongoose.connect(MONGO_URI || process.env.MONGO_URI!);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit process with failure
   }
 };
 
