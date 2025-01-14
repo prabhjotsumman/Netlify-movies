73d65045f7deda511952ff97912b4b50a9db0cba
prabhjotsumman <prabhjot.nith@gmail.com>
2025-01-14 14:18:10 -0700

    React boilerplate code

---
+++ frontend/src/constants.ts
@@ -0,0 +1,6 @@
+export const API_BASE_URL = "/api";
+
+export const API_MOVIES_ENDPOINT = API_BASE_URL + '/movies';
+export const API_ACTORS_ENDPOINT = API_BASE_URL + '/actors';
+export const API_PRODUCERS_ENDPOINT = API_BASE_URL + '/producers';
+
