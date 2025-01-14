58502bf17ced7d45977e968b7ea96bc8a360f989
prabhjotsumman <prabhjot.nith@gmail.com>
2025-01-14 14:18:43 -0700

    components: <MovieForm/> added

---
+++ frontend/src/components/MovieForm/MovieForm.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useMovieForm } from "../../hooks/useMovieForm";
+import MultiSelect from "../MultiSelect/MultiSelect";
+import AddActor from "../AddActor/AddActor";
+import AddProducer from "../AddProducer/AddProducer";
+import { Movie } from "../../reducers/movieSlice";
+
+import "./AddMovie.css";
+import "./MovieForm.css";
+
+interface MovieFormProps {
+  initialMovie?: Movie | null;
+  mode: "add" | "edit";
+  onCancel: () => void;
+}
+
+const MovieForm: React.FC<MovieFormProps> = ({ initialMovie, mode, onCancel }) => {
+  const {
+    movie,
+    setMovie,
+    selectedActors,
+    setSelectedActors,
+    selectedProducer,
+    setSelectedProducer,
+    actors,
+    producers,
+    handleSubmit,
+    handleNewActor,
+    handleDeleteActorFromDatabase,
+    handleDeleteProducerFromDatabase,
+  } = useMovieForm({ initialMovie, mode, onCancel });
+
+  return (
+    <div className="movie-form-container">
+      <h2>{mode === "add" ? "Add New Movie" : "Edit Movie"}</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-row">
+          <input
+            type="text"
+            placeholder="Movie Name"
+            value={movie.name}
+            onChange={(e) => setMovie({ ...movie, name: e.target.value })}
+          />
+          <input
+            type="number"
+            placeholder="Year"
+            value={movie.year}
+            onChange={(e) =>
+              setMovie({ ...movie, year: parseInt(e.target.value, 10) })
+            }
+          />
+        </div>
+        <textarea
+          placeholder="Plot"
+          className="input-plot"
+          value={movie.plot}
+          onChange={(e) => setMovie({ ...movie, plot: e.target.value })}
+        />
+
+        <MultiSelect
+          title="Actors"
+          options={actors}
+          selectedOptions={selectedActors}
+          setSelectedOptions={setSelectedActors}
+          multiple
+          onItemDelete={handleDeleteActorFromDatabase}
+        />
+
+        <AddActor onActorAdded={handleNewActor} />
+
+        <MultiSelect
+          title="Producer"
+          options={producers}
+          selectedOptions={selectedProducer}
+          setSelectedOptions={setSelectedProducer}
+          onItemDelete={handleDeleteProducerFromDatabase}
+        />
+        <AddProducer />
+
+        <button
+          type="submit"
+          disabled={
+            !movie.name ||
+            !movie.year ||
+            !movie.plot ||
+            !selectedActors.length ||
+            !selectedProducer.length
+          }
+        >
+          {mode === "add" ? "Add Movie" : "Save Changes"}
+        </button>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default React.memo(MovieForm);
